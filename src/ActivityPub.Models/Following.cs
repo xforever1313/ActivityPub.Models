f069f3dd16d1846456a011560a8df0def690666b
Seth Hendrick <seth@shendrick.net>
2022-12-11 15:46:43 -0500

    Added Following and FollowingCollection.

---
+++ src/ActivityPub.Models/Following.cs
@@ -0,0 +1,41 @@
+﻿//
+//          Copyright Seth Hendrick 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Text.Json.Serialization;
+
+namespace ActivityPub.Models
+{
+    public record class Following
+    {
+        [JsonPropertyName( "@context" )]
+        public string Context => "https://www.w3.org/ns/activitystreams";
+
+        [JsonPropertyName( "summary" )]
+        public string Summary => $"{this.Actor ?? ""} followed {this.Object ?? ""}.";
+
+        [JsonPropertyName( "type" )]
+        public string Type => "Follow";
+
+        [JsonPropertyName( "id" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string? Id { get; init; }
+
+        /// <summary>
+        /// Usually the person doing the following.
+        /// </summary>
+        [JsonPropertyName( "actor" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string? Actor { get; init; }
+
+        /// <summary>
+        /// Usually the person being followed.
+        /// </summary>
+        [JsonPropertyName( "object" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string? Object { get; init; }
+    }
+}
---
+++ src/ActivityPub.Models/FollowingCollection.cs
@@ -0,0 +1,45 @@
+﻿//
+//          Copyright Seth Hendrick 2022.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+
+using System.Text.Json.Serialization;
+
+namespace ActivityPub.Models
+{
+    public record class FollowingCollection
+    {
+        [JsonPropertyName( "@context" )]
+        public string Context => "https://www.w3.org/ns/activitystreams";
+
+        [JsonPropertyName( "summary" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public string? Summary { get; init; }
+
+        [JsonPropertyName( "type" )]
+        public string Type => "OrderedCollection";
+
+        [JsonPropertyName( "totalItems" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public int? TotalItems
+        {
+            get
+            {
+                if( this.Following is null )
+                {
+                    return null;
+                }
+                else
+                {
+                    return this.Following.Length;
+                }
+            }
+        }
+
+        [JsonPropertyName( "orderedItems" )]
+        [JsonIgnore( Condition = JsonIgnoreCondition.WhenWritingNull )]
+        public Following[]? Following { get; init; }
+    }
+}
